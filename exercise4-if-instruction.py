eef86293f9ede42ac06aa1d69cebf677fbdffdbd
marcusj773@gmail.com <marcusj773@gmail.com>
2023-08-26 03:09:51 +0000

    exercise4-if-instruction

---
+++ exercise4-if-instruction.py
@@ -0,0 +1,9 @@
+user_age = int(input('What is your age? '))
+if user_age > 30:
+    print('You are over 30 years old')
+    print('Sorry, you do not qualify')
+    # Must indent code 4 spaces
+else:
+    print('You are 30 years old or younger')
+    print('Congrattulations, you qualify!')
+
